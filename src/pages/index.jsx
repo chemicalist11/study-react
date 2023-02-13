8072c164e08ffd885bc90baf1f7769be7ec6be78
koike <crutchyawarakatennis2121@gmail.com>
2023-02-14 02:45:14 +0900

    ✨ 講座用のuseEffectを導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,25 +1,25 @@
 import Head from "next/head";
-import { Inter } from "@next/font/google";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useCallback } from "react";
-
-const inter = Inter({ subsets: ["latin"] });
+import { useEffect } from "react";
 
 export default function Home() {
-  const handleClick = useCallback((e) => {
-    console.log(e.target);
-    e.preventDefault();
+  useEffect(() => {
+    document.body.style.backgroundColor = "lightblue";
+    return () => {
+      document.body.style.backgroundColor = "";
+    };
   }, []);
+
   return (
     <>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-      <a href="/about" onClick={handleClick}>
+      {/* <a href="/about" onClick={handleClick}>
         ボタン
-      </a>
+      </a> */}
       <Main page="index" />
     </>
   );
