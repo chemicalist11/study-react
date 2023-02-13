880182b11d9e3717f97f0ced34013bb6e8aa08a2
koike <crutchyawarakatennis2121@gmail.com>
2023-02-14 01:27:08 +0900

    ⚗️ 不要な記述でいつか消しますが、試しにクリックイベントを書いてみました。

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,16 +2,24 @@ import Head from "next/head";
 import { Inter } from "@next/font/google";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
+import { useCallback } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const handleClick = useCallback((e) => {
+    console.log(e.target);
+    e.preventDefault();
+  }, []);
   return (
     <>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
       <Main page="index" />
     </>
   );
