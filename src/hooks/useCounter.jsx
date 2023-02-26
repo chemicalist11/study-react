208b48bf558a07dafbf3278ca2eba1bd7e61f83f
koike <crutchyawarakatennis2121@gmail.com>
2023-02-27 00:20:34 +0900

    🎨 Custom Hooksを使ってうまくまとめました。

---
+++ src/hooks/useCounter.jsx
@@ -0,0 +1,18 @@
+const { useState, useCallback } = require("react");
+
+export const useCounter = () => {
+  const [count, setCount] = useState(0);
+  const [isShow, setIsShow] = useState(true);
+
+  const handleClick = useCallback(() => {
+    if (count < 10) {
+      setCount((prevCount) => prevCount + 1);
+    }
+  }, [count]);
+
+  const handleDisplay = useCallback(() => {
+    setIsShow((prevIsShow) => !prevIsShow);
+  }, []);
+
+  return { count, isShow, handleClick, handleDisplay };
+};
