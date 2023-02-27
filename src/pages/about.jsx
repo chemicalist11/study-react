5965764fc7bc5f70062bcee68a3665008bbcc2fe
koike <crutchyawarakatennis2121@gmail.com>
2023-02-28 00:26:28 +0900

    ✨ ページ間のStateのリフトアップについて勉強

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,16 +2,16 @@ import Head from "next/head";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
 
-import { useCounter } from "src/hooks/useCounter";
-import { useInputArray } from "src/hooks/useInputArray";
-import { useBgLightBlue } from "src/hooks/useBgLightBlue";
-
-export default function About() {
-  const { count, isShow, handleClick, handleDisplay } = useCounter();
-  const { text, array, handleChange, handleAdd } = useInputArray();
-
-  useBgLightBlue();
-
+export default function About({
+  count,
+  isShow,
+  handleClick,
+  handleDisplay,
+  text,
+  array,
+  handleChange,
+  handleAdd,
+}) {
   return (
     <>
       <Head>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,16 +1,17 @@
 import Head from "next/head";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useCounter } from "src/hooks/useCounter";
-import { useInputArray } from "src/hooks/useInputArray";
-import { useBgLightBlue } from "src/hooks/useBgLightBlue";
 
-export default function Home() {
-  const { count, isShow, handleClick, handleDisplay } = useCounter();
-  const { text, array, handleChange, handleAdd } = useInputArray();
-
-  useBgLightBlue();
-
+export default function Home({
+  count,
+  isShow,
+  handleClick,
+  handleDisplay,
+  text,
+  array,
+  handleChange,
+  handleAdd,
+}) {
   return (
     <>
       <Head>
